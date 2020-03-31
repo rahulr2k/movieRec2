72d634f695917e83693924a90683db8a70219fa0
Rahul Babu K <rahulbabu101@gmail.com>
2020-03-31 17:23:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,46 +2,19 @@ from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
 
-#import re
 import pandas as pd
 import numpy as np
-#from sklearn.feature_extraction.text import CountVectorizer
 
 from nltk.tokenize import word_tokenize
 def token(text):
     tokenized_word=word_tokenize(text)
     return tokenized_word
 movies = pd.read_csv('MPR.csv')
-print("tokenization started")
 movies.keywords = movies.keywords.astype(str).apply(token)
-print("tokenization completed , starting similarity engine")
 processed_keywords = movies.keywords
 
 movies.originalTitle  = movies.originalTitle.astype(str).apply(lambda x : x.replace("'", ''))
 titlelist = movies.originalTitle.values.tolist()
-
-
-from gensim.corpora.dictionary import Dictionary
-from gensim.models.tfidfmodel import TfidfModel
-
-with open('tfidf3.pkl', 'rb') as f:
-        tfidf = pickle.load(f) #create tfidf model of the corpus
-
-with open('dictionary3.pkl', 'rb') as f:
-        dictionary = pickle.load(f)
-
-with open('tfidfcorpus3.pkl', 'rb') as f:
-        tfidfcorpus = pickle.load(f) 
-
- 
-        #create corpus where the corpus is a bag of words for each document
-corpus = [dictionary.doc2bow(doc) for doc in processed_keywords] 
-
-from gensim.similarities import MatrixSimilarity
-        # Create the similarity data structure. This is the most important part where we get the similarities between the movies.
-sims = MatrixSimilarity(tfidfcorpus, num_features=len(dictionary))  
-
-print("similarity generation completed")
 
 app = Flask(__name__)
  
